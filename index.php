03c1b7984e0778441a35f447852e5433d1bdb597
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-01-27 12:40:50 +0530

    index.php added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>CODE Website | EncredX
+    </title>
+</head>
+<body>
+    <h1>Click below to go to respective sites:</h1>
+    <p><a href="encredx/code2k19">CODE Website</a></p>
+    <p><a href="encredx/encredx">EncredX</a></p>
+
+</body>
+</html>
