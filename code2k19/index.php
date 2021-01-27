2aad591ce7aa4b71327559778d565114dac343fc
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-01-27 12:39:59 +0530

    path

--- code2k19/index.php
+++ code2k19/index.php
@@ -14,7 +14,7 @@
 <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet"> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<link rel= "stylesheet" href="/encredx/code2k19/styles/style.css"/>
+<link rel= "stylesheet" href="./styles/style.css"/>
 <script src="scripts/script.js"></script>
 </head>
 <body style="background-color:black;">
